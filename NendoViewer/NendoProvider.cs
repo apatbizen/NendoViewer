5244df11b231e04a72e815862c9d9f836b6df416
apatbizen <sunrise.seto285@gmail.com>
2020-01-19 11:31:50 +0900

    追加したAssertで期待通り動作するよう実装

--- NendoViewer/NendoProvider.cs
+++ NendoViewer/NendoProvider.cs
@@ -12,6 +12,7 @@ namespace NendoViewer {
         /// <param name="date">評価する日付</param>
         /// <returns>指定した日を含む年度</returns>
         public int GetYearFromDate(DateTime date) {
+            if (date.Month <= 3) return date.Year - 1;
             return date.Year;
         }
     }
